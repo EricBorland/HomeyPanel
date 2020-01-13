28b0fcf1eea23c960ab22072cb1772174205dbe4
BORLAND ACOSTA Eric <eric.borlandacosta@soprasteria.com>
2020-01-13 12:30:04 +0100

    feat(header): added date

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+const ONE_SECOND = 1000;
+
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
@@ -7,9 +9,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
+  now: number;
+
   constructor() { }
 
   ngOnInit() {
+    setInterval(() => { this.now = Date.now() }, ONE_SECOND);
   }
 
 }
