82f9dfbd35a3e18f76d6e07be6294ac6708760f5
Eric Borland <eric.borland@gmail.com>
2020-01-13 12:02:24 +0100

    fix(device): updates listeners

--- src/app/services/homey-api.service.ts
+++ src/app/services/homey-api.service.ts
@@ -9,6 +9,7 @@ const CLIENT_ID = '5cbb504da1fc782009f52e46';
 const CLIENT_SECRET = 'gvhs0gebgir8vz8yo2l0jfb49u9xzzhrkuo1uvs8';
 const REDIRECT_URL = 'http://localhost';
 const TOKEN = 'eyJfX2F0aG9tX2FwaV90eXBlIjoiQXRob21DbG91ZEFQSS5Ub2tlbiIsInRva2VuX3R5cGUiOiJiZWFyZXIiLCJhY2Nlc3NfdG9rZW4iOiIwMTQ2YjQ5YTA1MWY0ZDU0NWI5ODAxOTU2YTAxOTRjYTM2NzcyMDFkIiwiZXhwaXJlc19hdCI6IjIwMTktMTItMjhUMTE6MTY6MTkuOTU0WiIsInJlZnJlc2hfdG9rZW4iOiI2NDYzMzEzYWM2MGUxOGY1MzI2NDk5ODkxZTE3MjBlMzViODQzMDk1In0%3D';
+const MAX_LISTENERS = 100;
 
 @Injectable({
   providedIn: 'root'
@@ -85,6 +86,7 @@ export class HomeyApiService {
   async syncDevices() {
     this.requireHomey();
     this.devices = await this.homey.devices.getDevices();
+    this.homey.devices.setMaxListeners(MAX_LISTENERS);
     this.addDevicesListeners(this.devices);
     return this.devices;
   }
@@ -104,6 +106,7 @@ export class HomeyApiService {
   addDevicesListeners(devices) {
     for (const id in devices) {
       const device = devices[id];
+      device.setMaxListeners(MAX_LISTENERS);
       if (device.ui && device.ui.quickAction) {
         device.makeCapabilityInstance(device.ui.quickAction);
       }
